e5ad5e267182162db26f60d2c3721ff605650c43
siqiuchen <Siqiu.Chen@dentsplysirona.com>
2023-06-05 19:28:42 +0800

    first commit

---
+++ cmd/server.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// serverCmd represents the server command
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Launches the server",
+	Long: `Launches the server. For example:
+	proxy server -p 8080
+ `,
+
+	Run: runServer,
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	//serverCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	serverCmd.Flags().IntP("port", "p", 8080, "Port to listen on")
+}
+
+func runServer(cmd *cobra.Command, args []string) {
+	fmt.Println("server called")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "chatgpt-api-proxy/cmd"
+
+func main() {
+	cmd.Execute()
+}
