6cce779b4cb6715d860904aa7e3945694ddf2089
aqiu <819110812@qq.com>
2023-06-06 22:34:13 +0800

    refactor code

--- README.md
+++ README.md
@@ -19,8 +19,19 @@ ChatGPT API Proxy支持以下功能：
 ## Railway部署
 [![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template/ZweBXA)
 
+在railway中需要配置的环境变量有：
+```markdown
+OPENAI_API_KEY
+PORT
+```
+
 ## Vercel部署
 [![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fnext.js%2Ftree%2Fcanary%2Fexamples%2Fhello-world)
+
+在vercel中需要配置的环境变量有：
+```markdown
+OPENAI_API_KEY
+```
 
 # 贡献
 
--- pkg/api/openai/completion.go
+++ pkg/api/openai/completion.go
@@ -1,18 +1,29 @@
 package api
 
 import (
+	"bytes"
+	"chatgpt-api-proxy/config"
 	"chatgpt-api-proxy/internal/constant"
-	"chatgpt-api-proxy/internal/openai"
 	"chatgpt-api-proxy/pkg/httphelper"
+	"encoding/json"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pkg/errors"
 )
 
 var completionModels = []string{
 	"text-davinci-003",
 	"babage",
 }
+
+type CompletionModel string
+
+const (
+	Davinci CompletionModel = "text-davinci-003"
+	Babbage CompletionModel = "babbage"
+)
+
+const completionBaseURL = "https://api.openai.com/v1/completions"
 
 func InitCompletionRouter(r *gin.Engine) {
 	api := r.Group("/api/openai")
@@ -34,13 +45,39 @@ type CompletionRequest struct {
 	BestOf           int            `json:"best_of"`
 	LogitBias        map[string]int `json:"logit_bias"`
 	UserID           string         `json:"user"`
+	Stop             string         `json:"stop"`
+}
+
+// CompletionResponse is a response from the completion endpoint.
+type CompletionResponse struct {
+	ID      string   `json:"id"`
+	Object  string   `json:"object"`
+	Created int64    `json:"created"`
+	Model   string   `json:"model"`
+	Choices []Choice `json:"choices"`
+	Usage   Usage    `json:"usage"`
+}
+
+// Usage is a usage from the completion endpoint.
+type Usage struct {
+	PromptTokens     int `json:"prompt_tokens"`
+	CompletionTokens int `json:"completion_tokens"`
+	TotalTokens      int `json:"total_tokens"`
+}
+
+// Choice is a choice from the completion endpoint.
+type Choice struct {
+	Text         string      `json:"text"`
+	Index        int         `json:"index"`
+	Logprobs     interface{} `json:"logprobs"`
+	FinishReason string      `json:"finish_reason"`
 }
 
 func HandleCompletion(c *gin.Context) {
 	// parse request
 	request := CompletionRequest{}
-	err := c.Bind(&request)
-	if err != nil {
+
+	if err := c.Bind(&request); err != nil {
 		httphelper.WrapperError(c, constant.NewBaseError(constant.InvalidRequestError, err.Error()))
 		return
 	}
@@ -52,23 +89,8 @@ func HandleCompletion(c *gin.Context) {
 	}
 
 	// call openai client
-	client := openai.NewClient()
-	response, err := client.Completion(c, &openai.CompletionRequest{
-		Model:       openai.CompletionModel(request.Model),
-		MaxTokens:   request.MaxTokens,
-		Prompt:      request.Prompt,
-		Temperature: request.Temperature,
-	})
-	if err != nil {
-		if errors.Is(err, constant.Error(constant.GatewayTimeoutError)) {
-			httphelper.WrapperError(c, constant.NewBaseError(constant.GatewayTimeoutError, err.Error()))
-			return
-		}
-		httphelper.WrapperError(c, constant.NewBaseError(constant.InternalError, err.Error()))
-		return
-	}
-
-	httphelper.WrapperSuccess(c, response)
+	client := NewClient()
+	client.SendCompletionRequest(c, request)
 }
 
 func isSupportedModel(model string) bool {
@@ -78,4 +100,79 @@ func isSupportedModel(model string) bool {
 		}
 	}
 	return false
+}
+
+// Client is a client for OpenAI API.
+type Client struct {
+	httpClient *http.Client
+	APIKey     string
+}
+
+// NewClient returns a new OpenAI client.
+func NewClient() *Client {
+	apiKey := config.Store.OpenAI.APIKey
+	httpClient := &http.Client{}
+
+	return &Client{
+		APIKey:     apiKey,
+		httpClient: httpClient,
+	}
+}
+
+func (c *Client) SendCompletionRequest(ctx *gin.Context, request CompletionRequest) {
+	if request.Model == "" {
+		request.Model = string(Davinci)
+	}
+
+	if !isSupportedModel(request.Model) {
+		httphelper.WrapperError(ctx, constant.NewBaseError(constant.InvalidRequestError, "not supported model"))
+		return
+	}
+
+	payload, err := json.Marshal(request)
+	if err != nil {
+		httphelper.WrapperError(ctx, constant.NewBaseError(constant.InternalError, err.Error()))
+		return
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, completionBaseURL, bytes.NewReader(payload))
+	if err != nil {
+		httphelper.WrapperError(ctx, constant.NewBaseError(constant.InternalError, err.Error()))
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+c.APIKey)
+
+	resp, err := c.httpClient.Do(req)
+
+	if err != nil {
+		httphelper.WrapperError(ctx, constant.NewBaseError(constant.InternalError, err.Error()))
+		return
+	}
+
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		switch resp.StatusCode {
+		case http.StatusUnauthorized:
+			httphelper.WrapperError(ctx, constant.NewBaseError(constant.AuthenticationError, err.Error()))
+		case http.StatusTooManyRequests:
+			httphelper.WrapperError(ctx, constant.NewBaseError(constant.TooManyRequestsError, err.Error()))
+		case http.StatusServiceUnavailable:
+			httphelper.WrapperError(ctx, constant.NewBaseError(constant.ServiceUnavailableError, err.Error()))
+		case http.StatusGatewayTimeout:
+			httphelper.WrapperError(ctx, constant.NewBaseError(constant.GatewayTimeoutError, err.Error()))
+		default:
+			httphelper.WrapperError(ctx, constant.NewBaseError(constant.InternalError, err.Error()))
+		}
+	}
+
+	var completionResponse CompletionResponse
+	err = json.NewDecoder(resp.Body).Decode(&completionResponse)
+	if err != nil {
+		httphelper.WrapperError(ctx, constant.NewBaseError(constant.JSONUnmarshalError, err.Error()))
+	}
+
+	httphelper.WrapperSuccess(ctx, resp)
 }
