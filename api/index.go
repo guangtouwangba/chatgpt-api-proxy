f41276d5fdb0964fd06b29fdc849ee409c542091
aqiu <819110812@qq.com>
2023-06-14 22:25:01 +0800

    refactor code

--- api/index.go
+++ api/index.go
@@ -1,18 +1,12 @@
 package api
 
 import (
-	"chatgpt-api-proxy/config"
-	"chatgpt-api-proxy/internal/db"
+	"chatgpt-api-proxy/pkg/server"
 	"net/http"
-
-	"chatgpt-api-proxy/pkg/router"
 )
 
 // Handler is the entrypoint for the vercel serverless function.
 func Handler(w http.ResponseWriter, r *http.Request) {
-	_ = config.NewConfigStore()
-	engine := router.NewRouter()
-	// init db
-	db.SetUpDatabase()
+	server.InitServer()
 	engine.ServeHTTP(w, r)
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -4,6 +4,7 @@ import (
 	"chatgpt-api-proxy/config"
 	"chatgpt-api-proxy/internal/db"
 	"chatgpt-api-proxy/pkg/router"
+	"github.com/gin-gonic/gin"
 
 	"github.com/sirupsen/logrus"
 )
@@ -11,11 +12,16 @@ import (
 // RunServer starts the server.
 func RunServer() {
 	// init config
+	store, r := InitServer()
+	_ = r.Run(store.GetServerPort())
+}
+
+func InitServer() (*config.Config, *gin.Engine) {
 	store := config.NewConfigStore()
 	// init db
 	db.SetUpDatabase()
 
 	r := router.NewRouter()
 	logrus.Infof("start server at %s", store.Server.Port)
-	_ = r.Run(store.GetServerPort())
+	return store, r
 }
