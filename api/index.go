bb6770a15651aacfc957afb71c58a99b029781ff
aqiu <819110812@qq.com>
2023-06-14 22:23:17 +0800

    update vercel index

--- api/index.go
+++ api/index.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"chatgpt-api-proxy/config"
+	"chatgpt-api-proxy/internal/db"
 	"net/http"
 
 	"chatgpt-api-proxy/pkg/router"
@@ -8,7 +10,9 @@ import (
 
 // Handler is the entrypoint for the vercel serverless function.
 func Handler(w http.ResponseWriter, r *http.Request) {
+	_ = config.NewConfigStore()
 	engine := router.NewRouter()
-
+	// init db
+	db.SetUpDatabase()
 	engine.ServeHTTP(w, r)
 }
